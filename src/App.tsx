67aa673e82870e22fe5ac8557df47d7d59489796
Clinton Evans <study@getctrl.co.nz>
2024-02-16 06:19:56 +1300

    add expense list with delete button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import ListGroup from './components/ListGroup'
+import { useState } from 'react'
+import ExpenseList from './expense-tracker/components/ExpenseList'
 
 const App = () => {
-  const items = ['USA', 'Canada', 'Germany', 'Japan', 'Australia']
-
-  const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: 'Groceries', amount: 50, category: 'Food' },
+    { id: 2, description: 'Gas', amount: 30, category: 'Transportation' },
+    {
+      id: 3,
+      description: 'Movie tickets',
+      amount: 20,
+      category: 'Entertainment',
+    },
+    { id: 4, description: 'Restaurant', amount: 40, category: 'Food' },
+  ])
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Countries"
-        onSelectItem={handleSelectItem}
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
       />
     </div>
   )
